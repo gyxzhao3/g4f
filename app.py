e05da044d9cefa66efc0af21a39e1246b660c74f
gyxzhao2 <167040479+gyxzhao2@users.noreply.github.com>
2024-08-20 22:20:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import os
 from g4f.api import run_api
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 1337))
     run_api(port=port)
